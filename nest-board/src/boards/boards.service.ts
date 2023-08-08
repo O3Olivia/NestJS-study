223627f5773fc3aaa2bc72a2290afed89c002219
O3Olivia <cherishtmwu@gmail.com>
2023-08-09 01:06:31 +0900

    Board CRUD중 R 추가

--- nest-board/src/boards/boards.service.ts
+++ nest-board/src/boards/boards.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards = []; // private을 사용하지 않으면, 다름 컴포넌트에서 해당 배열을 수정할 수 있어서 차단하기 위해 사용
+
+  getAllBoards() {
+    // 이 함수를 호출하면,
+    return this.boards; // 위의 보드 배열에 있는 값을 return한다.
+  }
+}

05c49dda6ddf42547297ac449024dec1152b620c
O3Olivia <cherishtmwu@gmail.com>
2023-08-09 02:01:27 +0900

    타입 추가

--- nest-board/src/boards/boards.service.ts
+++ nest-board/src/boards/boards.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
 export class BoardsService {
-  private boards = []; // private을 사용하지 않으면, 다름 컴포넌트에서 해당 배열을 수정할 수 있어서 차단하기 위해 사용
+  private boards: Board[] = []; // private을 사용하지 않으면, 다름 컴포넌트에서 해당 배열을 수정할 수 있어서 차단하기 위해 사용
 
-  getAllBoards() {
+  getAllBoards(): Board[] {
     // 이 함수를 호출하면,
     return this.boards; // 위의 보드 배열에 있는 값을 return한다.
   }

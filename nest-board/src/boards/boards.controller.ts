31d252b06f6fcaf116f41377d5e896349308be09
O3Olivia <cherishtmwu@gmail.com>
2023-08-10 17:40:17 +0900

    특정 게시물 삭제 추가

--- nest-board/src/boards/boards.controller.ts
+++ nest-board/src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -21,5 +21,10 @@ export class BoardsController {
   @Post()
   getBoardById(@Param('id') id: string): Board {
     return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
   }
 }

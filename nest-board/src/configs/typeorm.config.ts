1300544ca024afdaf87655daca8cbda80b5833a1
O3Olivia <cherishtmwu@gmail.com>
2023-08-12 02:13:48 +0900

    update: board repository 생성

--- nest-board/src/configs/typeorm.config.ts
+++ nest-board/src/configs/typeorm.config.ts
@@ -1,6 +1,7 @@
 // typeorm 설정 파일 -> app.module.ts에서 import해주면 된다.
 
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Board } from 'src/boards/board.entity';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
@@ -9,6 +10,6 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   username: 'postgres',
   password: 'postgres',
   database: 'board-app',
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  entities: [__dirname + '/../**/*.entity.{js,ts}', Board],
   synchronize: true,
 };
